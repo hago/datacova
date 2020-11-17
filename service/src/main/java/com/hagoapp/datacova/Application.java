25835e07bc09e9eb0607592e91fc1c274b0faaf6
Chaojun Sun <suncjs@hagoapp.com>
2020-11-17 10:47:07 +0800

    add a application level get /set data accessor

--- service/src/main/java/com/hagoapp/datacova/Application.java
+++ service/src/main/java/com/hagoapp/datacova/Application.java
@@ -6,6 +6,9 @@ import com.hagoapp.datacova.command.Start;
 import com.hagoapp.datacova.command.Stop;
 import picocli.CommandLine;
 
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 /**
  * The entry of application. Based on command line arguments, user can start / stop services or execute a task.
  */
@@ -14,10 +17,20 @@ import picocli.CommandLine;
 })
 public class Application {
 
+    private static Map<String, Object> internalData = new ConcurrentHashMap<>();
+
     public static void main(String[] args) {
         Application app = new Application();
         CommandLine cli = new CommandLine(app);
         cli.setExecutionStrategy(app::executionStrategy).execute(args);
+    }
+
+    public static void setData(String key, Object data) {
+        internalData.put(key, data);
+    }
+
+    public static Object getData(String key) {
+        return internalData.get(key);
     }
 
     private int executionStrategy(CommandLine.ParseResult parseResult) {
