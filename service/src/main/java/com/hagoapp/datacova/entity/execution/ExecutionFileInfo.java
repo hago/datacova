f713f08af88ee0e58056979c530d3cf25fd34c54
Chaojun Sun <suncjs@hagoapp.com>
2020-12-30 17:09:08 +0800

    add copyright

--- service/src/main/java/com/hagoapp/datacova/entity/execution/ExecutionFileInfo.java
+++ service/src/main/java/com/hagoapp/datacova/entity/execution/ExecutionFileInfo.java
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2020.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
 package com.hagoapp.datacova.entity.execution;
 
 import com.fasterxml.jackson.jr.ob.JSON;
