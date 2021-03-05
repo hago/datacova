4bee1d0026525aebe1ced62ce1dd1b0f550b2062
Chaojun Sun <suncjs@hagoapp.com>
2021-03-05 13:17:43 +0800

    fix logger

--- service/src/main/java/com/hagoapp/datacova/CoVaLogger.java
+++ service/src/main/java/com/hagoapp/datacova/CoVaLogger.java
@@ -9,11 +9,15 @@
 package com.hagoapp.datacova;
 
 import com.hagoapp.datacova.config.CoVaConfig;
+import com.hagoapp.datacova.config.LoggingConfig;
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.core.Layout;
 import org.apache.logging.log4j.core.Logger;
 import org.apache.logging.log4j.core.LoggerContext;
 import org.apache.logging.log4j.core.appender.ConsoleAppender;
+import org.apache.logging.log4j.core.appender.RollingFileAppender;
+import org.apache.logging.log4j.core.appender.rolling.DefaultRolloverStrategy;
+import org.apache.logging.log4j.core.appender.rolling.TimeBasedTriggeringPolicy;
 import org.apache.logging.log4j.core.layout.PatternLayout;
 
 import java.io.Serializable;
@@ -28,6 +32,7 @@ public class CoVaLogger {
             } else {
                 initLogger();
             }
+            logger.info("logger configured");
         }
         return logger;
     }
@@ -37,9 +42,7 @@ public class CoVaLogger {
         Layout<? extends Serializable> layout = PatternLayout.newBuilder().withAlwaysWriteExceptions(true)
                 .withPattern("%d{DEFAULT_MICROS}\t%-5level\t%msg%n")
                 .withConfiguration(LoggerContext.getContext().getConfiguration()).build();
-        logger.getAppenders().values().forEach(appender -> {
-            logger.removeAppender(appender);
-        });
+        logger.getAppenders().values().forEach(appender -> logger.removeAppender(appender));
         ConsoleAppender appender = ConsoleAppender.createDefaultAppenderForLayout(layout);
         appender.start();
         logger.addAppender(appender);
@@ -47,6 +50,36 @@ public class CoVaLogger {
     }
 
     private static void initLogger() {
-        //TODO
+        logger = LoggerContext.getContext().getRootLogger();
+        LoggingConfig config = CoVaConfig.getConfig().getLogging();
+        Layout<? extends Serializable> layout = PatternLayout.newBuilder().withAlwaysWriteExceptions(true)
+                .withPattern(config.getMessagePattern())
+                .withConfiguration(LoggerContext.getContext().getConfiguration()).build();
+        logger.getAppenders().values().forEach(appender -> logger.removeAppender(appender));
+        if (config.isLog2Console()) {
+            ConsoleAppender appender = ConsoleAppender.createDefaultAppenderForLayout(layout);
+            appender.start();
+            logger.addAppender(appender);
+        }
+        if (config.isLog2File()) {
+            RollingFileAppender appender = RollingFileAppender.newBuilder()
+                    .setName(config.getLogFilePath())
+                    .withFileName(config.getLogFilePath())
+                    .withFilePattern(config.getRollingLogFilePattern())
+                    .withStrategy(DefaultRolloverStrategy.newBuilder().build())
+                    .withPolicy(TimeBasedTriggeringPolicy.newBuilder().build())
+                    .setLayout(layout)
+                    .build();
+            appender.start();
+            logger.addAppender(appender);
+        }
+        logger.setLevel(Level.ALL);
+        Level level = Level.getLevel(config.getLevel());
+        if (level == null) {
+            logger.error("Incorrect level value: {}, ALL is used", config.getLevel());
+        } else {
+            logger.info("log level is set to {}", level);
+            logger.setLevel(level);
+        }
     }
 }
