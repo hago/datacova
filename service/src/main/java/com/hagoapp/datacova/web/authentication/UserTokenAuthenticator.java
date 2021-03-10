2f08ab695f9f32e2f3d26956a2ba233fbdc34d23
Chaojun Sun <suncjs@hagoapp.com>
2021-03-10 17:32:13 +0800

    fix

--- service/src/main/java/com/hagoapp/datacova/web/authentication/UserTokenAuthenticator.java
+++ service/src/main/java/com/hagoapp/datacova/web/authentication/UserTokenAuthenticator.java
@@ -1,21 +1,26 @@
 package com.hagoapp.datacova.web.authentication;
 
+import com.hagoapp.datacova.CoVaLogger;
 import com.hagoapp.datacova.user.UserInfo;
+import com.hagoapp.datacova.util.web.AuthUtils;
 import io.vertx.ext.web.RoutingContext;
+import org.apache.logging.log4j.Logger;
 
 public class UserTokenAuthenticator extends Authenticator {
 
-    private static final String HEADER_TOKEN = "USER_TOKEN";
-
     private UserInfo userInfo;
+    private final Logger logger = CoVaLogger.getLogger();
 
     @Override
     protected boolean validateAuthentication(RoutingContext routingContext) {
-        String token = routingContext.request().getHeader(HEADER_TOKEN);
+        String token = AuthUtils.Companion.getCurrentToken(routingContext);
         if (token == null) {
             return false;
         }
         userInfo = UserTokenManager.getManager().findToken(token);
+        if (userInfo == null) {
+            logger.warn("User for token {} not found", token);
+        }
         return userInfo != null;
     }
 
