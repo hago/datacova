945a4fefe4e33189caa21be16a38d55c763a0d63
Chaojun Sun <suncjs@hagoapp.com>
2021-03-11 17:24:44 +0800

    fix api url when proxied

--- web/portal/config/dev.env.js
+++ web/portal/config/dev.env.js
@@ -4,7 +4,7 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  SERVICE_BASE_URL: '"http://127.0.0.1:6786"',
+  SERVICE_BASE_URL: '"http://127.0.0.1:8080"',
   PARTNER_ID: '"5e893b34dd9227b36629327d2afe380"',
   PARTNER_KEY: '"2cb7737ab13d9abca880f8cf4f6a840c2611ae96"'
 })
