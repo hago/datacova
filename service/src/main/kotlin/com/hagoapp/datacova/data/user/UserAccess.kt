f5705391561795b26f8aa95a8a9f9a7de8e92e61
Chaojun Sun <suncjs@hagoapp.com>
2021-03-15 11:17:46 +0800

    add redis layer of user access

---
+++ service/src/main/kotlin/com/hagoapp/datacova/data/user/UserAccess.kt
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2020.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+package com.hagoapp.datacova.data.user
+
+import com.hagoapp.datacova.data.RedisCacheReader
+import com.hagoapp.datacova.data.RedisCacheReader.GenericLoader
+import com.hagoapp.datacova.data.UserData
+import com.hagoapp.datacova.user.UserInfo
+
+class UserAccess {
+    companion object {
+
+        private const val USER_INFO = "UserInfo"
+
+        @JvmStatic
+        fun getUser(id: Long): UserInfo? {
+            return RedisCacheReader.readCachedData(USER_INFO, 3600, object : GenericLoader<UserInfo> {
+                override fun perform(params: Array<out Any?>): UserInfo? {
+                    UserData().use {
+                        return it.findUser(params[0] as Long)
+                    }
+                }
+            }, UserInfo::class.java, id)
+        }
+    }
+}
