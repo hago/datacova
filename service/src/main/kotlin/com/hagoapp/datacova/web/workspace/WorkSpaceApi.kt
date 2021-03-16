11943b2dfef55eb2817fa9fce075261dfbc55f8c
Chaojun Sun <suncjs@hagoapp.com>
2021-03-16 15:39:58 +0800

    move out addMember

--- service/src/main/kotlin/com/hagoapp/datacova/web/workspace/WorkSpaceApi.kt
+++ service/src/main/kotlin/com/hagoapp/datacova/web/workspace/WorkSpaceApi.kt
@@ -1,8 +1,6 @@
 package com.hagoapp.datacova.web.workspace
 
-import com.google.gson.Gson
 import com.google.gson.GsonBuilder
-import com.google.gson.reflect.TypeToken
 import com.hagoapp.datacova.data.user.UserAccess
 import com.hagoapp.datacova.data.user.WorkspaceCache
 import com.hagoapp.datacova.data.workspace.WorkSpaceData
@@ -110,33 +108,5 @@ class WorkSpaceApi {
         wk.modifyBy = user.id
         val workspace0 = WorkSpaceData().updateWorkSpace(wk)
         ResponseHelper.sendResponse(routeContext, HttpResponseStatus.OK, mapOf("code" to 0, "data" to workspace0))
-    }
-
-    @WebEndPoint(
-        methods = [HttpMethod.PUT],
-        path = "/api/workspace/:id/member/:type/add",
-        authTypes = [AuthType.UserToken]
-    )
-    fun addWorkspaceMember(routeContext: RoutingContext) {
-        val id = routeContext.request().getParam("id").toIntOrNull()
-        val type = routeContext.request().getParam("type").toIntOrNull()
-        if ((id == null) || (type == null)) {
-            ResponseHelper.respondError(routeContext, HttpResponseStatus.BAD_REQUEST, "invalid parameter")
-            return
-        }
-        val user = Authenticator.getUser(routeContext)
-        val wk = WorkspaceCache.getWorkspace(id)
-        if ((wk == null) || ((wk.ownerId != user.id)) && (WorkspaceCache.getWorkspaceUserInRoles(
-                id,
-                listOf(WorkSpaceUserRole.Admin)
-            ).isEmpty())
-        ) {
-            ResponseHelper.respondError(routeContext, HttpResponseStatus.FORBIDDEN, "denied")
-            return
-        }
-        val token = object : TypeToken<List<Long>>() {}
-        val idList = Gson().fromJson<List<Long>>(routeContext.bodyAsString, token.type)
-        WorkSpaceData().addMemberForWorkspace(id, WorkSpaceUserRole.parseInt(type), idList)
-        ResponseHelper.sendResponse(routeContext, HttpResponseStatus.OK)
     }
 }
