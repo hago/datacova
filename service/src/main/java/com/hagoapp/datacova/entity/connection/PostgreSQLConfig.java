41d81ac01039aee359f1a00d75c6d4bfafdf417e
Chaojun Sun <suncjs@hagoapp.com>
2021-03-17 13:20:26 +0800

    add pg config

---
+++ service/src/main/java/com/hagoapp/datacova/entity/connection/PostgreSQLConfig.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright (c) 2021.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+package com.hagoapp.datacova.entity.connection;
+
+public class PostgreSQLConfig extends ConnectionConfig {
+    private String host;
+    private int port;
+    private String userName;
+    private String password;
+    private String dbName;
+
+    public int getPort() {
+        return port <= 0 ? 5432 : port;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
+    }
+
+    public String getHost() {
+        return host;
+    }
+
+    public void setHost(String host) {
+        this.host = host;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getDbName() {
+        return dbName;
+    }
+
+    public void setDbName(String dbName) {
+        this.dbName = dbName;
+    }
+
+    @Override
+    public int getType() {
+        return 0;
+    }
+}
