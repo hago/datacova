ab3b4a3546f8c44535d2499496f4c76c5ec27300
Chaojun Sun <suncjs@hagoapp.com>
2021-03-22 16:59:35 +0800

    enable ingest option edit

--- web/portal/src/components/task/TaskActionIngest.vue
+++ web/portal/src/components/task/TaskActionIngest.vue
@@ -1,38 +1,70 @@
 <template>
-  <div class="form-row margin-left: 5px">
-    <div class="form-group col-6">
-      <label for="connection">Connection</label>
-      <select v-model="action.connectionId" class="form-control" id="connection" v-on:change="switchConnection()">
-        <option v-for="connection in connections" :key="connection.id" :value="connection.id">
-          {{ connection.name }}
-        </option>
-      </select>
+  <div>
+    <div class="form-row margin-left: 5px">
+      <div class="form-group col-6">
+        <label for="connection">Connection</label>
+        <select v-model="action.connectionId" class="form-control" id="connection" v-on:change="switchConnection()">
+          <option v-for="connection in connections" :key="connection.id" :value="connection.id">
+            {{ connection.name }}
+          </option>
+        </select>
+      </div>
+      <div class="form-group col-3">
+        <label for="schema">Target Schema</label>
+        <select v-model="action.ingestOptions.targetSchema" class="form-control" id="connection">
+          <option v-for="(schema, index) in schemas" v-bind:key="index" v-bind:value="schema">
+            {{ schema }}
+          </option>
+          <option disabled value=undefined>Choose Schema</option>
+        </select>
+      </div>
+      <div class="form-group col-3">
+        <label for="table">Target Table</label>
+        <span style="float: right; margin-right: 5px">
+          <img src="@/assets/create.png" width="20px" title="Create New Table" class="clickable"
+            v-if="!createTable" v-on:click="toggleCreateTable(true)" />
+          <img src="@/assets/choose.png" width="20px" title="Choose Existing Table" class="clickable"
+            v-if="createTable" v-on:click="toggleCreateTable(false)"  />
+        </span>
+        <select v-model="action.ingestOptions.targetTable" class="form-control" id="selectTable" v-if="!createTable">
+          <option v-for="(item, index) in tablesInSchema" v-bind:key="index" v-bind:value="item.tableName">
+            {{ item.tableName }}
+          </option>
+          <option disabled value=undefined>Choose Table</option>
+        </select>
+        <input type="text" v-model="action.ingestOptions.targetTable" class="form-control" id="inputTable"
+          v-if="createTable" ref="inputTable" />
+      </div>
     </div>
-    <div class="form-group col-3">
-      <label for="schema">Target Schema</label>
-      <select v-model="action.ingestOptions.targetSchema" class="form-control" id="connection">
-        <option v-for="(schema, index) in schemas" v-bind:key="index" v-bind:value="schema">
-          {{ schema }}
-        </option>
-        <option disabled value=undefined>Choose Schema</option>
-      </select>
-    </div>
-    <div class="form-group col-3">
-      <label for="table">Target Table</label>
-      <span style="float: right; margin-right: 5px">
-        <img src="@/assets/create.png" width="20px" title="Create New Table" class="clickable"
-           v-if="!createTable" v-on:click="toggleCreateTable(true)" />
-        <img src="@/assets/choose.png" width="20px" title="Choose Existing Table" class="clickable"
-          v-if="createTable" v-on:click="toggleCreateTable(false)"  />
-      </span>
-      <select v-model="action.ingestOptions.targetTable" class="form-control" id="selectTable" v-if="!createTable">
-        <option v-for="(item, index) in tablesInSchema" v-bind:key="index" v-bind:value="item.tableName">
-          {{ item.tableName }}
-        </option>
-        <option disabled value=undefined>Choose Table</option>
-      </select>
-      <input type="text" v-model="action.ingestOptions.targetTable" class="form-control" id="inputTable"
-        v-if="createTable" ref="inputTable" />
+    <div class="form-row margin-left: 5px">
+      <div class="form-group col-3">
+        <div class="form-check">
+          <input class="form-check-input" type="checkbox" id="addBatch" v-model="action.ingestOptions.addBatch">
+          <label class="form-check-label" for="addBatch">
+            Add batch column
+          </label>
+        </div>
+      </div>
+      <div class="form-group col-3">
+        <input type="text" v-model="action.ingestOptions.batchColumnName" class="form-control" id="batchColumnName"
+          ref="batchColumnName" :disabled="!action.ingestOptions.addBatch" placeholder="BatchId" />
+      </div>
+      <div class="form-group col-3">
+        <div class="form-check">
+          <input class="form-check-input" type="checkbox" id="clearTable" v-model="action.ingestOptions.clearTable">
+          <label class="form-check-label" for="clearTable">
+            Purge table
+          </label>
+        </div>
+      </div>
+      <div class="form-group col-3">
+        <div class="form-check">
+          <input class="form-check-input" type="checkbox" id="createTable" v-model="action.ingestOptions.createTableIfNeeded">
+          <label class="form-check-label" for="createTable">
+            Create table automatically if not existed
+          </label>
+        </div>
+      </div>
     </div>
   </div>
 </template>
