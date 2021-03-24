848b79f5cbd8678d3392c506ec391f972c3ebdca
Chaojun Sun <suncjs@hagoapp.com>
2021-03-24 16:37:33 +0800

    use auto discovery for action executor

--- service/src/main/kotlin/com/hagoapp/datacova/execution/TaskActionExecutorFactory.kt
+++ service/src/main/kotlin/com/hagoapp/datacova/execution/TaskActionExecutorFactory.kt
@@ -8,20 +8,40 @@
 
 package com.hagoapp.datacova.execution
 
+import com.hagoapp.datacova.Application
 import com.hagoapp.datacova.CoVaException
+import com.hagoapp.datacova.CoVaLogger
 import com.hagoapp.datacova.entity.action.TaskAction
+import com.hagoapp.datacova.entity.action.TaskActionType
 import com.hagoapp.datacova.execution.executor.BaseTaskActionExecutor
+import org.reflections.Reflections
+import org.reflections.scanners.SubTypesScanner
+import java.lang.reflect.Constructor
 
 class TaskActionExecutorFactory {
     companion object {
 
+        private val actionExecutorMap = mutableMapOf<TaskActionType, Constructor<out BaseTaskActionExecutor>>()
+        private val logger = CoVaLogger.getLogger()
+
+        init {
+            val r = Reflections(Application::class.java.packageName, SubTypesScanner())
+            r.getSubTypesOf(BaseTaskActionExecutor::class.java).forEach { executor ->
+                try {
+                    val creator = executor.getConstructor()
+                    val type = creator.newInstance().getActionType()
+                    actionExecutorMap[type] = creator
+                    logger.info("Action executor for {} registered", type)
+                } catch (e: Exception) {
+                    logger.error("Action executor {}} register failed", executor.canonicalName)
+                }
+            }
+        }
+
         fun createTaskActionExecutor(action: TaskAction): BaseTaskActionExecutor {
-            return when (action.type) {
-//                TaskActionType.DataImporting -> ImportExecutor()
-//                TaskActionType.Distributing -> DistributeExecutor()
-//                TaskActionType.Verifying -> VerifyExecutor()
-//                TaskActionType.IDLE -> IdleExecutor()
-                else -> throw CoVaException("executor for action ${action.type} not implemented")
+            return when (val creator = actionExecutorMap[action.type]) {
+                null -> throw CoVaException("executor for action ${action.type} not implemented")
+                else -> creator.newInstance()
             }
         }
     }
