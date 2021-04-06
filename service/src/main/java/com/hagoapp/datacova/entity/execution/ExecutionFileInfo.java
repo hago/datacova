0a1fd4a1f7dbc633109f659ee9cd1404e3c4bc94
Chaojun Sun <suncjs@hagoapp.com>
2021-04-06 11:24:18 +0800

    fix fileinfo deserialization

--- service/src/main/java/com/hagoapp/datacova/entity/execution/ExecutionFileInfo.java
+++ service/src/main/java/com/hagoapp/datacova/entity/execution/ExecutionFileInfo.java
@@ -12,6 +12,7 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.hagoapp.datacova.JsonStringify;
 import com.hagoapp.f2t.datafile.FileInfo;
+import com.hagoapp.f2t.datafile.FileInfoReader;
 
 import java.io.IOException;
 import java.util.Map;
@@ -46,10 +47,13 @@ public class ExecutionFileInfo implements JsonStringify {
         this.fileInfo = fileInfo;
     }
 
+    @SuppressWarnings("unchecked")
     public static ExecutionFileInfo getFileInfo(String s) throws IOException {
         Gson gson = new GsonBuilder().create();
         ExecutionFileInfo eai = gson.fromJson(s, ExecutionFileInfo.class);
         Map<String, Object> map = JSON.std.mapFrom(s);
+        Map<String, Object> subMap = (Map<String, Object>) map.get("fileInfo");
+        eai.fileInfo = FileInfoReader.Companion.json2FileInfo(subMap);
         return eai;
     }
 }
