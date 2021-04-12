f116535f8776e4923e0a135e2fb28e693d413110
Chaojun Sun <suncjs@hagoapp.com>
2021-04-12 17:45:06 +0800

    impl multiple user fetching and caching

--- service/src/main/kotlin/com/hagoapp/datacova/data/user/UserCache.kt
+++ service/src/main/kotlin/com/hagoapp/datacova/data/user/UserCache.kt
@@ -7,24 +7,57 @@
 
 package com.hagoapp.datacova.data.user
 
+import com.google.gson.Gson
+import com.hagoapp.datacova.config.CoVaConfig
 import com.hagoapp.datacova.data.RedisCacheReader
 import com.hagoapp.datacova.data.RedisCacheReader.GenericLoader
+import com.hagoapp.datacova.data.redis.JedisManager
 import com.hagoapp.datacova.user.UserInfo
 
 class UserCache {
     companion object {
 
         private const val USER_INFO = "UserInfo"
+        private const val USER_INFO_CACHE_TIME = 3600
 
         @JvmStatic
         fun getUser(id: Long): UserInfo? {
-            return RedisCacheReader.readCachedData(USER_INFO, 3600, object : GenericLoader<UserInfo> {
+            return RedisCacheReader.readCachedData(USER_INFO, USER_INFO_CACHE_TIME, object : GenericLoader<UserInfo> {
                 override fun perform(params: Array<out Any?>): UserInfo? {
                     UserData().use {
                         return it.findUser(params[0] as Long)
                     }
                 }
             }, UserInfo::class.java, id)
+        }
+
+        @JvmStatic
+        fun batchGetUser(idList: List<Long>): List<UserInfo?> {
+            val list = idList.map { id ->
+                RedisCacheReader.readCachedData(USER_INFO, 3600, object : GenericLoader<UserInfo> {
+                    override fun perform(params: Array<out Any?>): UserInfo? {
+                        return null
+                    }
+                }, UserInfo::class.java, id)
+            }.toMutableList()
+            println(list)
+            val nullUsers = list.mapIndexed { i, info ->
+                if (info == null) Pair(idList[i], i) else null
+            }.filterNotNull().toMap()
+            println(nullUsers)
+            JedisManager(CoVaConfig.getConfig().redis).use {
+                val jedis = it.jedis
+                val gson = Gson()
+                UserData().batchGetUser(nullUsers.keys).forEach { info ->
+                    if (info != null) {
+                        val index = nullUsers.getValue(info.id)
+                        list[index] = info
+                        val key = RedisCacheReader.createCacheKey(USER_INFO, info.id)
+                        jedis.setex(key, USER_INFO_CACHE_TIME, gson.toJson(info))
+                    }
+                }
+            }
+            return list
         }
     }
 }
