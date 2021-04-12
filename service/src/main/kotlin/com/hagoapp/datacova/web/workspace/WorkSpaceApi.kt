30fd08c22b94b6d37c18a842382622b673c4eb99
Chaojun Sun <suncjs@hagoapp.com>
2021-04-12 17:46:55 +0800

    impl task execution list fetch api per workspace

--- service/src/main/kotlin/com/hagoapp/datacova/web/workspace/WorkSpaceApi.kt
+++ service/src/main/kotlin/com/hagoapp/datacova/web/workspace/WorkSpaceApi.kt
@@ -1,6 +1,9 @@
 package com.hagoapp.datacova.web.workspace
 
 import com.google.gson.GsonBuilder
+import com.hagoapp.datacova.data.execution.TaskExecutionCache
+import com.hagoapp.datacova.data.execution.TaskExecutionCache.Companion.TASK_EXECUTION_DEFAULT_PAGE_SIZE
+import com.hagoapp.datacova.data.execution.TaskExecutionData
 import com.hagoapp.datacova.data.user.UserCache
 import com.hagoapp.datacova.data.workspace.WorkspaceCache
 import com.hagoapp.datacova.data.workspace.WorkSpaceData
@@ -107,5 +110,53 @@ class WorkSpaceApi {
         val workspace0 = WorkSpaceData().updateWorkSpace(wk)
         WorkspaceCache.clearMyWorkspaces(user.id)
         ResponseHelper.sendResponse(routeContext, HttpResponseStatus.OK, mapOf("code" to 0, "data" to workspace0))
+    }
+
+    @WebEndPoint(
+        methods = [HttpMethod.GET],
+        path = "/api/workspace/:id/executions",
+        authTypes = [AuthType.UserToken]
+    )
+    fun getExecutionList(context: RoutingContext) {
+        val id = context.pathParam("id").toInt()
+        doGetExecutionRage(context, id, 0, TASK_EXECUTION_DEFAULT_PAGE_SIZE)
+    }
+
+    @WebEndPoint(
+        methods = [HttpMethod.GET],
+        path = "/api/workspace/:id/executions/:size",
+        authTypes = [AuthType.UserToken]
+    )
+    fun getExecutionSizedList(context: RoutingContext) {
+        val id = context.pathParam("id").toInt()
+        val size = context.pathParam("size").toInt()
+        doGetExecutionRage(context, id, 0, size)
+    }
+
+    @WebEndPoint(
+        methods = [HttpMethod.GET],
+        path = "/api/workspace/:id/executions/:start/:size",
+        authTypes = [AuthType.UserToken]
+    )
+    fun getExecutionRange(context: RoutingContext) {
+        val id = context.pathParam("id").toInt()
+        val size = context.pathParam("size").toInt()
+        val start = context.pathParam("start").toInt()
+        doGetExecutionRage(context, id, start, size)
+    }
+
+    private fun doGetExecutionRage(context: RoutingContext, workspaceId: Int, start: Int, size: Int) {
+        val user = Authenticator.getUser(context)
+        if (!WorkspaceUserRoleUtil.isUser(user, workspaceId)) {
+            ResponseHelper.respondError(context, HttpResponseStatus.FORBIDDEN, "access denied")
+            return
+        }
+        val executions = TaskExecutionCache.getExecutionsOfWorkspace(workspaceId, start, size)
+        ResponseHelper.sendResponse(
+            context, HttpResponseStatus.OK, mapOf(
+                "code" to 0,
+                "data" to mapOf("executions" to executions)
+            )
+        )
     }
 }
