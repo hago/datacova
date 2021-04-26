4b9f8f27c916481cc0b50759439f91b05b1acb45
Chaojun Sun <suncjs@hagoapp.com>
2021-04-26 11:21:43 +0800

    add ishtml and pass test

---
+++ service/src/main/kotlin/com/hagoapp/datacova/util/text/TextUtils.kt
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2021.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+package com.hagoapp.datacova.util.text
+
+class TextUtils {
+    companion object {
+        @JvmStatic
+        fun isHtml(input: String): Boolean {
+            val singleQuoteStart = input.indexOf("'")
+            val doubleQuoteStart = input.indexOf('"')
+            val r = Regex("<[a-zA-Z\\\\]+>", setOf(RegexOption.DOT_MATCHES_ALL))
+            val tagStart = r.find(input)?.range?.start ?: -1
+            return when {
+                tagStart < 0 -> false
+                (singleQuoteStart < 0) && (doubleQuoteStart < 0) -> true
+                (singleQuoteStart < 0) && (doubleQuoteStart >= 0) -> doubleQuoteStart > tagStart
+                (singleQuoteStart >= 0) && (doubleQuoteStart < 0) -> singleQuoteStart > tagStart
+                singleQuoteStart > doubleQuoteStart -> doubleQuoteStart > tagStart
+                else -> singleQuoteStart > tagStart
+            }
+        }
+    }
+}
---
+++ service/src/test/kotlin/com/hagoapp/datacova/TextUtilsTest.kt
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2021.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+package com.hagoapp.datacova
+
+import com.hagoapp.datacova.util.text.TextUtils
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+class TextUtilsTest {
+    @Test
+    fun testIsHtml() {
+        mapOf(
+            "This is a sentence" to false,
+            "<a>This is a sentence" to true,
+            "'<a>This is a sentence'" to false,
+            "\"<a>This is a sentence\"" to false,
+            "\"<a>This is a 'sentence'\"" to false,
+            "'<a>This is a \"sentence\"'" to false,
+            "<a>This is a \"sentence\"" to true,
+            "<a>This is a 'sentence'" to true,
+            "<a>'This is a \"sentence\"'" to true,
+            "<a>\"This is a 'sentence'\"" to true
+        ).forEach { (text, result) ->
+            println("evaluate $text, $result expected")
+            Assertions.assertEquals(TextUtils.isHtml(text), result)
+        }
+    }
+}
