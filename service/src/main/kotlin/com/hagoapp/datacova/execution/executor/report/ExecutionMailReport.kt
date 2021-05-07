685a107e7b48d009fb94af30dcd039931041a135
Chaojun Sun <suncjs@hagoapp.com>
2021-05-07 22:50:18 +0800

    load email template using template manager

--- service/src/main/kotlin/com/hagoapp/datacova/execution/executor/report/ExecutionMailReport.kt
+++ service/src/main/kotlin/com/hagoapp/datacova/execution/executor/report/ExecutionMailReport.kt
@@ -11,11 +11,21 @@ package com.hagoapp.datacova.execution.executor.report
 import com.hagoapp.datacova.CoVaLogger
 import com.hagoapp.datacova.entity.execution.ExecutionDetail
 import com.hagoapp.datacova.entity.execution.TaskExecution
+import com.hagoapp.datacova.util.text.TemplateManager
 
-class ExecutionMailReport: ExecutionReport {
+class ExecutionMailReport : ExecutionReport {
     private val logger = CoVaLogger.getLogger()
 
+    companion object {
+        const val EXECUTION_REPORT_MAIL_TEMPLATE = "exec_report"
+    }
+
     override fun sendReport(execution: TaskExecution, detail: ExecutionDetail) {
-        logger.debug("send report mail")
+        val template = TemplateManager.getManager().getTemplate(EXECUTION_REPORT_MAIL_TEMPLATE)
+        if (template == null) {
+            logger.error("Execution mail report template configuration incorrect")
+            return
+        }
+
     }
 }
