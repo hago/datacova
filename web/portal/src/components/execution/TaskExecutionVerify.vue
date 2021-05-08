684014d6ee5181e3d72af8d83ce269f0f1dfee31
Chaojun Sun <suncjs@hagoapp.com>
2021-05-08 15:34:44 +0800

    display data messages in web UI

--- web/portal/src/components/execution/TaskExecutionVerify.vue
+++ web/portal/src/components/execution/TaskExecutionVerify.vue
@@ -1,8 +1,25 @@
 <template>
   <div>
-    <div class="row" v-if="succeeded">
-      <div class="col">
-        <span class="linecount">{{ detail.lineCount}}</span> rows inserted in <span class="timeused">{{ timeused }}</span>.
+    <div  v-if="succeeded">
+      <div class="row">
+        <div class="col">
+          <span class="linecount">{{ detail.lineCount}}</span> rows verified in <span class="timeused">{{ timeused }}</span>.
+        </div>
+      </div>
+      <div class="row" v-if="hasDataMessage">
+        <h4 class="col">Following data failed to pass verification</h4>
+      </div>
+      <div class="row" v-if="hasDataMessage">
+        <div class="col-1">Line</div>
+        <div class="col-2">Field</div>
+        <div class="col-4">Value</div>
+        <div class="col-5">Description</div>
+      </div>
+      <div class="row" v-for="(message, i) in flatDataMessages" v-bind:key="i">
+        <div class="col-1">{{ message.line }}</div>
+        <div class="col-2">{{ message.field }}</div>
+        <div class="col-4">{{ message.value }}</div>
+        <div class="col-5">{{ message.descriptionExpected }}</div>
       </div>
     </div>
     <div class="row" v-if="!succeeded">
@@ -47,6 +64,21 @@ export default {
       } else {
         return err.detailMessage
       }
+    },
+    hasDataMessage: function () {
+      return Object.keys(this.actiondetail.dataMessages).length > 0
+    },
+    flatDataMessages: function () {
+      let flat = []
+      let keys = Object.keys(this.actiondetail.dataMessages).sort((a, b) => a - b)
+      keys.forEach(line => {
+        console.log(line)
+        let lineList = this.actiondetail.dataMessages[line]
+        lineList.forEach(message => {
+          flat.push(Object.assign({}, message, {line: line}))
+        })
+      })
+      return flat
     }
   }
 }
