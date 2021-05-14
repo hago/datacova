eca6bfdbf9b634e5c6ff62b56de0d55f8c9bd819
Chaojun Sun <suncjs@hagoapp.com>
2021-05-15 00:14:25 +0800

    add introduction in home page

--- web/portal/src/components/Index.vue
+++ web/portal/src/components/Index.vue
@@ -1,6 +1,30 @@
 <template>
-  <div style="height: 100%; width: 100%;">
-    <img src="@/assets/gear-loading.gif" v-if="loginStatus.user === undefined" class="loading"/>
+  <div>
+    <div class="row">
+      <div class="col-6">
+        <h1>What is Data<span class="brand">CoVa</span></h1>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-1"></div>
+      <div class="col-10">
+        <p class="description">
+          &nbsp;&nbsp;Data<span class="brand">CoVa</span> is a set of services to process data files.
+          It provides capability to:
+        </p>
+        <ul class="featrures">
+          <li>Extract tabular data from files</li>
+          <li>Detect column definitions and create table automatically</li>
+          <li>Ingest into table in databases</li>
+          <li>Validate data using customized restrictions</li>
+          <li>Distribute data to various destinations</li>
+        </ul>
+        <p class="description">
+          &nbsp;&nbsp;It supports all mainstream databases; supports distribution to multiple destinations; provides
+          many validation methods and pre-built rules, and providers flexible workspace and user managemnet.
+        </p>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -27,5 +51,18 @@ export default {
     margin: auto auto auto auto;
     width: 150px;
     height: 150px;
+}
+h1 {
+  text-align: center;
+  font-size: 500%
+}
+.brand {
+  color: red;
+}
+.description {
+  font-size: 280%
+}
+.description {
+  font-size: 240%
 }
 </style>
