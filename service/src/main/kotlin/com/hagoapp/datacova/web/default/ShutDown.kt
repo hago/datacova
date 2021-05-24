8241756538bfc4dc5c50a5908f2635b1b0059d82
Chaojun Sun <suncjs@hagoapp.com>
2021-05-24 15:14:21 +0800

    impl shutdown api

---
+++ service/src/main/kotlin/com/hagoapp/datacova/web/default/ShutDown.kt
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 2021.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+package com.hagoapp.datacova.web.default
+
+import com.hagoapp.datacova.config.CoVaConfig
+import com.hagoapp.datacova.execution.Service
+import com.hagoapp.datacova.util.http.RequestHelper
+import com.hagoapp.datacova.util.http.ResponseHelper
+import com.hagoapp.datacova.web.WebManager
+import com.hagoapp.datacova.web.annotation.WebEndPoint
+import com.hagoapp.datacova.web.authentication.AuthType
+import edazdarevic.commons.net.CIDRUtils
+import io.netty.handler.codec.http.HttpResponseStatus
+import io.vertx.core.http.HttpMethod
+import io.vertx.ext.web.RoutingContext
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import java.util.concurrent.atomic.AtomicBoolean
+
+class ShutDown {
+
+    companion object {
+        private var isShuttingDown: AtomicBoolean = AtomicBoolean(false)
+    }
+
+    @WebEndPoint(
+        methods = [HttpMethod.GET],
+        authTypes = [AuthType.Anonymous],
+        path = "/api/service/shutdown",
+        isBlocking = false
+    )
+    fun shutDown(context: RoutingContext) {
+        if (!canShutDown(context)) {
+            ResponseHelper.respondError(context, HttpResponseStatus.UNAUTHORIZED, "NOT Allowed")
+            return
+        }
+        if (isShuttingDown.acquire) {
+            ResponseHelper.sendResponse(context, HttpResponseStatus.OK)
+        }
+        isShuttingDown.set(true)
+        GlobalScope.launch {
+            Service.getExecutor().stopExecutionService()
+            WebManager.getManager().shutDownWebServer()
+        }
+        ResponseHelper.sendResponse(context, HttpResponseStatus.OK)
+    }
+
+    fun canShutDown(context: RoutingContext): Boolean {
+        val ip = RequestHelper.getRemoteIp(context)
+        return CoVaConfig.getConfig().web.privilegedIpAddresses.any {
+            if (it.contains('/')) CIDRUtils(it).isInRange(ip) else it.equals(ip)
+        }
+    }
+}
