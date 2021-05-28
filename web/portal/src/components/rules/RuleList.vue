a30e95d9f3ec840c400f24edc9e7c3d48e14b25a
Chaojun Sun <suncjs@hagoapp.com>
2021-05-28 15:28:00 +0800

    impl add/edit rule

--- web/portal/src/components/rules/RuleList.vue
+++ web/portal/src/components/rules/RuleList.vue
@@ -1,9 +1,26 @@
 <template>
-  <div></div>
+  <div class="card bg-dark">
+    <div class="card-header">
+      <h5>
+        <span>Validation Rules</span>
+        <span class="clickable" style="float: right" v-on:click="addRule()" title="New Connection">+</span>
+      </h5>
+    </div>
+    <div class="card-body">
+      <ul class="list-group">
+        <li class="list-group-item bg-dark" v-for="rule in rules" v-bind:key="rule.id">
+          <span v-on:click="editRule(rule.id)" class="clickable">{{ rule.name }}</span>
+          <span v-on:click="deleteRule(rule.id)" style="float: right" class="clickable"
+            v-if="rules.canDelete.indexOf(rule.id) >= 0">-</span>
+        </li>
+      </ul>
+    </div>
+  </div>
 </template>
 
 <script>
 import WorkspaceApiHelper from '@/apis/workspace.js'
+import router from '../../router'
 
 export default {
   name: 'RuleList',
@@ -19,6 +36,31 @@ export default {
     (new WorkspaceApiHelper()).getRules(this.workspace.workspace.id).then(rsp => {
       this.rules = rsp.data.data
     }).catch(err => console.log(err))
+  },
+  methods: {
+    addRule: function () {
+      router.push({
+        name: 'ValidationRule',
+        params: {
+          workspace: this.workspace,
+          wkid: this.workspace.workspace.id,
+          ruleid: 'new'
+        }
+      })
+    },
+    editRule: function (id) {
+      router.push({
+        name: 'ValidationRule',
+        params: {
+          workspace: this.workspace,
+          wkid: this.workspace.workspace.id,
+          ruleid: id
+        }
+      })
+    },
+    deleteRule: function (id) {
+      //
+    }
   }
 }
 </script>
