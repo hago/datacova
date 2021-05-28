d0bf882dd9d82dede7e3cba08376b9b993a52dfa
Chaojun Sun <suncjs@hagoapp.com>
2021-05-28 14:37:12 +0800

    add rule entity

---
+++ service/src/main/java/com/hagoapp/datacova/entity/action/verification/Rule.java
@@ -0,0 +1,94 @@
+/*
+ * Copyright (c) 2021.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+package com.hagoapp.datacova.entity.action.verification;
+
+import com.hagoapp.datacova.JsonStringify;
+
+public class Rule implements JsonStringify {
+    private long id;
+    private String name;
+    private String description;
+    private Configuration ruleConfig;
+    private Integer workspaceId;
+    private long addBy;
+    private long addTime;
+    private long modifyBy;
+    private long modifyTime;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Configuration getRuleConfig() {
+        return ruleConfig;
+    }
+
+    public void setRuleConfig(Configuration ruleConfig) {
+        this.ruleConfig = ruleConfig;
+    }
+
+    public Integer getWorkspaceId() {
+        return workspaceId;
+    }
+
+    public void setWorkspaceId(Integer workspaceId) {
+        this.workspaceId = workspaceId;
+    }
+
+    public long getAddBy() {
+        return addBy;
+    }
+
+    public void setAddBy(long addBy) {
+        this.addBy = addBy;
+    }
+
+    public long getAddTime() {
+        return addTime;
+    }
+
+    public void setAddTime(long addTime) {
+        this.addTime = addTime;
+    }
+
+    public long getModifyBy() {
+        return modifyBy;
+    }
+
+    public void setModifyBy(long modifyBy) {
+        this.modifyBy = modifyBy;
+    }
+
+    public long getModifyTime() {
+        return modifyTime;
+    }
+
+    public void setModifyTime(long modifyTime) {
+        this.modifyTime = modifyTime;
+    }
+}
