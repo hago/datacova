743b4758d5cfb42e37ef8061d8065ce2b9c7f6d0
Chaojun Sun <suncjs@hagoapp.com>
2021-06-03 14:08:19 +0800

    refactor web config

--- service/src/main/java/com/hagoapp/datacova/config/WebConfig.java
+++ service/src/main/java/com/hagoapp/datacova/config/WebConfig.java
@@ -8,38 +8,14 @@
 
 package com.hagoapp.datacova.config;
 
-import com.hagoapp.datacova.util.Utils;
-
-import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Configuration of web interfaces.
  */
-public class WebConfig {
-    private String bindIp = "127.0.0.1";
-    private int port = 6786;
-    private boolean allowCrossOriginResourceSharing = false;
-    private List<String> crossOriginSources = new ArrayList<>();
-    private List<String> privilegedIpAddresses = List.of(
-            "127.0.0.1",
-            "192.168.0.0/24",
-            "10.*.*.*"
-    );
-    private String tempDirectory = "./";
-    private String uploadTempDirectory = "./";
-    private long uploadSizeLimit = -1;
-    private boolean outputStackTrace = true;
+public class WebConfig extends BaseWebConfig {
     private List<WebSocketConfig> webSockets;
-    private String baseUrl;
-
-    public String getBaseUrl() {
-        return baseUrl;
-    }
-
-    public void setBaseUrl(String baseUrl) {
-        this.baseUrl = baseUrl;
-    }
+    private String privateKeyFile;
 
     public List<WebSocketConfig> getWebSockets() {
         return webSockets;
@@ -49,89 +25,29 @@ public class WebConfig {
         this.webSockets = webSockets;
     }
 
-    public boolean isOutputStackTrace() {
-        return outputStackTrace;
+    public String getPrivateKeyFile() {
+        return privateKeyFile;
     }
 
-    public void setOutputStackTrace(boolean outputStackTrace) {
-        this.outputStackTrace = outputStackTrace;
-    }
-
-    public String getBindIp() {
-        return bindIp;
-    }
-
-    public void setBindIp(String bindIp) {
-        this.bindIp = bindIp;
-    }
-
-    public int getPort() {
-        return port;
-    }
-
-    public void setPort(int port) {
-        this.port = port;
-    }
-
-    public boolean isAllowCrossOriginResourceSharing() {
-        return allowCrossOriginResourceSharing;
-    }
-
-    public void setAllowCrossOriginResourceSharing(boolean allowCrossOriginResourceSharing) {
-        this.allowCrossOriginResourceSharing = allowCrossOriginResourceSharing;
-    }
-
-    public List<String> getCrossOriginSources() {
-        return crossOriginSources;
-    }
-
-    public void setCrossOriginSources(List<String> crossOriginSources) {
-        this.crossOriginSources = crossOriginSources;
-    }
-
-    public List<String> getPrivilegedIpAddresses() {
-        return privilegedIpAddresses;
-    }
-
-    public void setPrivilegedIpAddresses(List<String> privilegedIpAddresses) {
-        this.privilegedIpAddresses = privilegedIpAddresses;
-    }
-
-    public String getTempDirectory() {
-        return tempDirectory != null ? tempDirectory : Utils.getSystemTemporaryDirectory();
-    }
-
-    public void setTempDirectory(String tempDirectory) {
-        this.tempDirectory = tempDirectory != null ? tempDirectory : Utils.getSystemTemporaryDirectory();
-    }
-
-    public String getUploadTempDirectory() {
-        return uploadTempDirectory != null ? uploadTempDirectory : Utils.getSystemTemporaryDirectory();
-    }
-
-    public void setUploadTempDirectory(String uploadTempDirectory) {
-        this.uploadTempDirectory = uploadTempDirectory != null ? uploadTempDirectory :
-                Utils.getSystemTemporaryDirectory();
-    }
-
-    public long getUploadSizeLimit() {
-        return uploadSizeLimit;
-    }
-
-    public void setUploadSizeLimit(long uploadSizeLimit) {
-        this.uploadSizeLimit = uploadSizeLimit;
+    public void setPrivateKeyFile(String privateKeyFile) {
+        this.privateKeyFile = privateKeyFile;
     }
 
     @Override
     public String toString() {
-        return "WebConfig{"
-                + "bindIp='" + bindIp + '\''
-                + ", port=" + port
-                + ", allowCrossOriginResourceSharing=" + allowCrossOriginResourceSharing
-                + ", crossOriginSources=" + crossOriginSources
-                + ", privilegedIpAddresses=" + privilegedIpAddresses
-                + ", tempDirectory='" + tempDirectory + '\''
-                + ", uploadTempDirectory='" + uploadTempDirectory + '\''
-                + ", uploadSizeLimit=" + uploadSizeLimit + '}';
+        return "WebConfig{" +
+                "bindIp='" + bindIp + '\'' +
+                ", port=" + port +
+                ", allowCrossOriginResourceSharing=" + allowCrossOriginResourceSharing +
+                ", crossOriginSources=" + crossOriginSources +
+                ", privilegedIpAddresses=" + privilegedIpAddresses +
+                ", tempDirectory='" + tempDirectory + '\'' +
+                ", uploadTempDirectory='" + uploadTempDirectory + '\'' +
+                ", uploadSizeLimit=" + uploadSizeLimit +
+                ", outputStackTrace=" + outputStackTrace +
+                ", baseUrl='" + baseUrl + '\'' +
+                ", webSockets=" + webSockets +
+                ", privateKeyFile='" + privateKeyFile + '\'' +
+                '}';
     }
 }
