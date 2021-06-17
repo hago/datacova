a5b16b52e69ccf8aa233c7f8f70237731ead43a9
Chaojun Sun <suncjs@hagoapp.com>
2021-06-17 16:35:54 +0800

    impl stop command

--- service/src/main/java/com/hagoapp/datacova/command/Stop.java
+++ service/src/main/java/com/hagoapp/datacova/command/Stop.java
@@ -9,12 +9,40 @@
 package com.hagoapp.datacova.command;
 
 import com.hagoapp.datacova.CoVaException;
+import com.hagoapp.datacova.CoVaLogger;
+import com.hagoapp.datacova.config.CoVaConfig;
+import org.slf4j.Logger;
 import picocli.CommandLine;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 @CommandLine.Command(name = "stop", description = "stop service")
 public class Stop extends CommandWithConfig {
+
+    @CommandLine.Option(names = {"-f", "--force"}, description = "force service to stop", arity = "0")
+    private boolean force = false;
+
+    private final Logger logger = CoVaLogger.getLogger();
+
     @Override
     public Integer call() throws CoVaException {
+        var config = CoVaConfig.getConfig().getWeb();
+        var url = String.format("http://%s:%d/api/service/shutdown", config.getBindIp(), config.getPort());
+        if (force) {
+            url += "/force";
+        }
+        var http = HttpClient.newHttpClient();
+        var request = HttpRequest.newBuilder(URI.create("")).GET().build();
+        try {
+            http.send(request, HttpResponse.BodyHandlers.ofString());
+            logger.info("Notification of stop has been sent to service");
+        } catch (IOException | InterruptedException e) {
+            logger.error("Messaging service to stop failed: {}", e.getMessage());
+        }
         return super.call();
     }
 }
