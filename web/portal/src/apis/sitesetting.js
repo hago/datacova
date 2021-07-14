a9ab05b84439a8399b4405d18f7c9a1be6354929
Chaojun Sun <suncjs@hagoapp.com>
2021-07-14 17:08:59 +0800

    refactor, save site settings to session

---
+++ web/portal/src/apis/sitesetting.js
@@ -0,0 +1,32 @@
+import SiteApiHelper from './siteapi.js'
+
+class SiteSetting {
+  constructor () {
+    if (!SiteSetting.instance) {
+      SiteSetting.instance = this
+    }
+    return SiteSetting.instance
+  }
+
+  async getSettings () {
+    let settingStr = sessionStorage.getItem('sitesetting')
+    return new Promise(
+      function (resolve, reject) {
+        if (settingStr !== null) {
+          console.log('sute setting info found')
+          resolve(JSON.parse(settingStr))
+        } else {
+          (new SiteApiHelper(process.env.SERVICE_BASE_URL)).loadsettings().then(rsp => {
+            settingStr = JSON.stringify(rsp.data.settings)
+            sessionStorage.setItem('sitesetting', settingStr)
+            resolve(rsp.data.settings)
+          }).catch(err => {
+            reject(err)
+          })
+        }
+      }
+    )
+  }
+}
+
+export default SiteSetting
