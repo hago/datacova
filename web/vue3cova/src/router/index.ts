bab6555c7a763a793e945f3468142465997a1af7
Chaojun Sun <suncjs@hagoapp.com>
2023-01-31 10:46:24 +0800

    impl basic before check in route

--- web/vue3cova/src/router/index.ts
+++ web/vue3cova/src/router/index.ts
@@ -1,13 +1,45 @@
+import { identityStore } from '@/stores/identity'
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import RouteErrorView from '@/components/auth/RouteErrorView.vue'
+
+declare module 'vue-router' {
+  interface RouteMeta {
+    login: {
+      requireLogin: boolean,
+      jumpToLogin: boolean
+    }
+  }
+}
+
+const DEFAULT_LOGIN_URL = "/login"
+const DEFAULT_ROUTE_ERROR_URL = "/error/route"
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
+
   routes: [
+    {
+      path: '/error/route',
+      name: 'route_error',
+      component: RouteErrorView,
+      meta: {
+        login: {
+          requireLogin: false,
+          jumpToLogin: false
+        }
+      }
+    },
     {
       path: '/',
       name: 'home',
-      component: HomeView
+      component: HomeView,
+      meta: {
+        login: {
+          requireLogin: true,
+          jumpToLogin: false
+        }
+      }
     },
     {
       path: '/about',
@@ -18,6 +50,32 @@ const router = createRouter({
       component: () => import('../views/AboutView.vue')
     }
   ]
+})
+
+router.beforeEach((to, from) => {
+  console.log('router.beforeEach', to, from)
+  if (to.meta.login === undefined) {
+    console.log('login meta is missed, assuming no auth needed ', to.fullPath)
+    return true
+  }
+  if (!to.meta.login.requireLogin) {
+    console.log('login is not required')
+    return true
+  }
+  console.log('login is required')
+  let user = identityStore().current
+  if (!user.isValidIdentity(user)) {
+    console.log('user login status is not found')
+    if (to.meta.login.jumpToLogin) {
+      console.log('redirect to default login page')
+      return DEFAULT_LOGIN_URL
+    } else {
+      console.log('stop routing')
+      return DEFAULT_ROUTE_ERROR_URL
+    }
+  }
+  console.log('user login status found')
+  return to
 })
 
 export default router
