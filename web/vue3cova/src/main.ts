8e09228a8a6ab8640bb51bb440616c35a996df26
Chaojun Sun <suncjs@hagoapp.com>
2023-02-01 11:22:13 +0800

    use naive-ui

--- web/vue3cova/src/main.ts
+++ web/vue3cova/src/main.ts
@@ -1,14 +1,16 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
-import App from './App.vue'
+//import App from './App.vue'
+import Cova from './Cova.vue'
 import router from './router'
+import naive from 'naive-ui'
 
 import './assets/main.css'
 
-const app = createApp(App)
+const app = createApp(Cova)
 
 app.use(createPinia())
-app.use(router)
-
-app.mount('#app')
+    .use(router)
+    .use(naive)
+    .mount('#app')
