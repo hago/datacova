cd4e87b124d9f21d613d539ede57e71f1bd88743
Chaojun Sun <suncjs@hagoapp.com>
2023-02-07 15:33:45 +0800

    fix identity store

--- web/vue3cova/src/stores/identity.ts
+++ web/vue3cova/src/stores/identity.ts
@@ -1,33 +1,44 @@
-import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
-import type Identity from '@/entities/identity'
+import Identity from '@/entities/identity'
 import { emptyIdentity } from '@/entities/identity'
 
 type LogonStatus = {
     current: Identity,
-    pseudo: Identity,
+    impersonated: Identity,
     loginTime: number,
     lastActiveTime: number
 }
 
+export function currentIdentity(status: LogonStatus): Identity {
+    return (status.impersonated != emptyIdentity) ? status.impersonated : status.current
+}
+
+export function currentActualIdentity(status: LogonStatus): Identity {
+    return status.current
+}
+
+export function isImpersonating(status: LogonStatus): boolean {
+    return status.current.id != status.impersonated.id
+}
+
 export const identityStore = defineStore('identity', {
     state: (): LogonStatus => ({
-        current: emptyIdentity,
-        pseudo: emptyIdentity,
+        current: new Identity(),
+        impersonated: new Identity(),
         loginTime: 0,
         lastActiveTime: 0
     }),
     actions: {
         impersonate(imperonatee: Identity) {
-            this.pseudo = imperonatee
+            this.impersonated = imperonatee
         },
         soptImpersonate() {
-            this.pseudo = emptyIdentity
+            this.impersonated = emptyIdentity
         },
         logout() {
             this.current = emptyIdentity
             this.loginTime = 0,
-            this.lastActiveTime = 0
+                this.lastActiveTime = 0
         },
         login(loginUser: Identity) {
             this.current = loginUser
