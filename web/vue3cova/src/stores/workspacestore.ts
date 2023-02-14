6af1f36c35f61d5705ee857f758536d22d648dda
Chaojun Sun <suncjs@hagoapp.com>
2023-02-14 17:24:02 +0800

    enable session storage for workspacestore

--- web/vue3cova/src/stores/workspacestore.ts
+++ web/vue3cova/src/stores/workspacestore.ts
@@ -2,42 +2,28 @@ import type { WorkspaceWithUser } from "@/api/workspaceapi";
 import { defineStore } from "pinia";
 
 type WorkspaceCache = {
-    workspaces: Map<number, WorkspaceWithUser>,
+    workspaces: WorkspaceWithUser[],
     selectedWorkspace: WorkspaceWithUser | null
 }
 
 export const workspaceStore = defineStore("workspaces", {
     state: (): WorkspaceCache => {
         return {
-            workspaces: new Map(),
+            workspaces: [],
             selectedWorkspace: null
         }
     },
     actions: {
         getWorkspace(id: number): WorkspaceWithUser | null {
-            let w = this.workspaces.get(id)
+            let w = this.workspaces[id]
             return w === undefined ? null : w
         },
         setWorkspace(workspace: WorkspaceWithUser) {
-            this.workspaces.set(workspace.workspace.id, workspace)
-        },
-        getSelectedWorkspace(): WorkspaceWithUser | null {
-            return this.selectedWorkspace
-        },
-        selectWorkspace(w: WorkspaceWithUser) {
-            this.selectedWorkspace = w
-        },
-        selectWorkspaceId(id: number) {
-            let w = this.workspaces.get(id)
-            if (w == undefined) {
-                console.log(`error: workspace with id ${id} not found in store`)
-                this.selectedWorkspace = null
-            } else {
-                this.selectedWorkspace = w
-            }
-        },
-        clearSelectedWorkspace() {
-            this.selectedWorkspace = null
+            this.workspaces[workspace.workspace.id] = workspace
+            console.log(`size: ${this.workspaces.length}`)
         }
+    },
+    persist: {
+        storage: sessionStorage
     }
 })
