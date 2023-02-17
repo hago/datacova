9239d4c4fc3fe7980a1a000fe373621b8a4df6c2
Chaojun Sun <suncjs@hagoapp.com>
2023-02-17 15:55:09 +0800

    show title and check admin

--- web/vue3cova/src/components/content/TaskInfo.vue
+++ web/vue3cova/src/components/content/TaskInfo.vue
@@ -1,14 +1,54 @@
 <script lang="ts">
-import { defineComponent, type PropType } from 'vue';
+import { defineComponent, reactive, type PropType } from 'vue';
 import type { Task } from '@/entities/task/task';
+import { currentIdentity, identityStore } from '@/stores/identitystore';
+import { isAdmin, isMaintainer } from '@/util/permission';
+import workspaceApiHelper from '@/api/workspaceapi';
+import { workspaceStore } from '@/stores/workspacestore';
+import type { WorkspaceWithUser } from "@/api/workspaceapi";
 
 export default defineComponent({
     name: "TaskInfo",
     props: {
-        task: Object as PropType<Task>
+        task: {
+            type: Object as PropType<Task>,
+            required: true
+        }
     },
     setup() {
-        return {}
+        return reactive({
+            canModify: false,
+            canDelete: false
+        })
+    },
+    mounted() {
+        this.updateWorkspaceTask()
+    },
+    update() {
+        this.updateWorkspaceTask()
+    },
+    methods: {
+        updateWorkspaceTask() {
+            let w = workspaceStore().getWorkspace(this.task.id)
+            if (w !== null) {
+                this.updateTask(w)
+            } else {
+                let user = currentIdentity(identityStore())
+                workspaceApiHelper.getWorksapce(user, this.task.workspaceId, {
+                    success: (rsp) => {
+                        workspaceStore().setWorkspace(rsp.data)
+                        this.updateTask(rsp.data)
+                    },
+                    fail: () => { }
+                })
+            }
+        },
+        updateTask(workspace: WorkspaceWithUser) {
+            console.log(workspace)
+            let user = currentIdentity(identityStore())
+            this.canModify = isAdmin(user, workspace) || isMaintainer(user, workspace)
+            this.canDelete = isAdmin(user, workspace)
+        }
     }
 })
 </script>
@@ -16,7 +56,21 @@ export default defineComponent({
 <template>
     <n-grid cols="2">
         <n-gi>
+            <input class="taskname" v-model="task.name" type="text" placeholder="Task Name" v-bind:readonly="!canModify" />
+        </n-gi>
+        <n-gi style="text-align: right;">
+            <n-button type="error" v-if="canDelete">Delete</n-button>
+        </n-gi>
+        <n-gi>
             <h2>{{ `Workspace ${task?.name}` }}</h2>
         </n-gi>
-    </n-grid>
+</n-grid>
 </template>
+
+<style scoped>
+.taskname {
+    font-weight: bold;
+    font-size: larger;
+    color: orange;
+}
+</style>
