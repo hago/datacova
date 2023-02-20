cf2c383a82a2a5a50ee3e22a0ebec14139cbccb4
Chaojun Sun <suncjs@hagoapp.com>
2023-02-20 17:04:03 +0800

    rename event

--- web/vue3cova/src/entities/events.ts
+++ web/vue3cova/src/entities/events.ts
@@ -1,3 +1,3 @@
 export const EVENT_TASK_DELETED = "taskDeleted"
-export const EVENT_REMOTE_API_ERROR = "apiError"
+export const EVENT_REMOTE_API_ERROR = "apiErrorOccurred"
 export const EVENT_LOGIN_STATUS_CHANGED = "loginStatusChanged"
