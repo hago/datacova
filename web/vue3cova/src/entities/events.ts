95dbeeff33009afc32dd171c3f241fb5cd2c5cfb
Chaojun Sun <suncjs@hagoapp.com>
2023-02-21 18:19:12 +0800

    add event for close recipients editor

--- web/vue3cova/src/entities/events.ts
+++ web/vue3cova/src/entities/events.ts
@@ -1,3 +1,6 @@
 export const EVENT_TASK_DELETED = "taskDeleted"
 export const EVENT_REMOTE_API_ERROR = "apiErrorOccurred"
 export const EVENT_LOGIN_STATUS_CHANGED = "loginStatusChanged"
+
+
+export const EVENT_TASKINFO_CLOSE_RECIPIENTS_EDITOR = "task_info_close_recipients_editor"
