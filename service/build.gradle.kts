71b0a7533b81fa9287854e93be9b79f8160b13cd
Chaojun Sun <suncjs@hagoapp.com>
2023-02-23 10:55:30 +0800

    adjust build files

--- service/build.gradle.kts
+++ service/build.gradle.kts
@@ -4,6 +4,12 @@ plugins {
     id("org.jetbrains.dokka") version("1.7.20")
 }
 
+subprojects{
+    apply(plugin = "java")
+    apply(plugin = "org.jetbrains.dokka").apply { version = "1.7.20" }
+    apply(plugin = "kotlin").apply { version = "1.7.20" }
+}
+
 group = "org.hagoapp.datacova"
 version = "1.0-SNAPSHOT"
 
