6bb180fa589d233e2a84177ee83661b58e105274
Chaojun Sun <suncjs@hagoapp.com>
2023-02-27 18:35:29 +0800

    draft dist configs

---
+++ web/vue3cova/src/components/task/distribute/ActionDistFtp.vue
@@ -0,0 +1,26 @@
+<script lang="ts">
+import type { Task, TaskAction } from '@/entities/task/task';
+import { defineComponent, type PropType } from 'vue';
+
+export default defineComponent({
+    props: {
+        action: {
+            type: Object as PropType<TaskAction>,
+            required: true
+        },
+        task: {
+            type: Object as PropType<Task>,
+            required: true
+        },
+        readonly: {
+            type: Boolean,
+            required: true
+        }
+    },
+    setup(props) {
+
+    }
+})
+</script>
+
+<template></template>
