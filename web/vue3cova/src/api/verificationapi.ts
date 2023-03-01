286bcb53e28db141c225bf1d897c8f5d0cafd56b
Chaojun Sun <suncjs@hagoapp.com>
2023-03-01 15:47:35 +0800

    ftp verify api

---
+++ web/vue3cova/src/api/verificationapi.ts
@@ -0,0 +1,28 @@
+import type Identity from "@/entities/identity"
+import type DistFtpConfiguration from "@/entities/task/dist/distconfigftp"
+import { fromFetchResponse, type BaseResponse } from "./baseresponse"
+import { addTokenHeader } from "./credential"
+
+export interface FtpVerificationResponse extends BaseResponse {
+    pwd: string
+}
+
+export class VerificationApi {
+    constructor() {
+        //
+    }
+
+    async verifyFtp(user: Identity, config: DistFtpConfiguration): Promise<FtpVerificationResponse> {
+        let headers = addTokenHeader(user)
+        headers.append("content-type", "application/json")
+        let p = await fetch("/api/distribute/verify/ftp", {
+            headers: headers,
+            method: "POST",
+            body: JSON.stringify(config)
+        })
+        return fromFetchResponse(p)
+    }
+}
+
+const verificationApi: VerificationApi = new VerificationApi()
+export default verificationApi
