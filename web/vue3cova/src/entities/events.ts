95fc3d30ed385e9bcddf0060f6e330427c89caea
Chaojun Sun <suncjs@hagoapp.com>
2023-03-09 10:37:24 +0800

    add connection select event

--- web/vue3cova/src/entities/events.ts
+++ web/vue3cova/src/entities/events.ts
@@ -5,4 +5,6 @@ export const EVENT_LOGIN_STATUS_CHANGED = "loginStatusChanged"
 export const EVENT_TASK_SELECTED = "task_selected"
 export const EVENT_TASKINFO_CLOSE_RECIPIENTS_EDITOR = "task_info_close_recipients_editor"
 
+export const EVENT_CONNECTION_SELECTED = "onConnectionSelected"
+
 export const EVENT_GLOBAL_DRAWER_NOTIFY = "global_drawer_botify"
