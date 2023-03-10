ab5bcbda9807e081e8eb5a0776ee6f48c57b0f39
Chaojun Sun <suncjs@hagoapp.com>
2023-03-10 15:41:33 +0800

    impl pg and mssql ui

--- web/vue3cova/src/components/connection/dbconfig/MsSqlComponent.vue
+++ web/vue3cova/src/components/connection/dbconfig/MsSqlComponent.vue
@@ -1,27 +1,44 @@
 <script lang="ts">
 import type { BaseDbConfig } from '@/entities/connection/dbconfigbase';
 import type { MsSqlConfig } from '@/entities/connection/mssqlconfig';
-import { defineComponent, reactive, type PropType } from 'vue';
+import { computed, defineComponent, reactive, type PropType } from 'vue';
 
 export default defineComponent({
     props: {
         config: {
             type: Object as PropType<BaseDbConfig>,
             required: true
-        }
+        },
+        editable: Boolean
     },
     setup(props) {
         let conf = props.config as MsSqlConfig
         return reactive({
-            conf
+            conf,
+            port: computed({
+                get: () => (conf.port.toString()),
+                set: (v: any) => {
+                    if (typeof (v) === 'string') {
+                        conf.port = parseInt(v)
+                    }
+                }
+            })
         })
     }
 })
 </script>
 
 <template>
-    
+    <n-grid cols="2">
+        <n-gi>
+            <span class="field">Host</span>
+            <n-input class="taskname" v-model:value="conf.host" type="text" placeholder="Host" :readonly="!editable" />
+        </n-gi>
+        <n-gi>
+            <span class="field">Port</span>
+            <n-input class="taskname" v-model:value="port" type="number" placeholder="port" :readonly="!editable" />
+        </n-gi>
+    </n-grid>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
