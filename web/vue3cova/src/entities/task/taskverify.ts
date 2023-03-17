d17193ca6f67713ca0a397d3bfcd0ae90f3bd30b
Chaojun Sun <suncjs@hagoapp.com>
2023-03-17 13:57:15 +0800

    def rule type strings and routines

--- web/vue3cova/src/entities/task/taskverify.ts
+++ web/vue3cova/src/entities/task/taskverify.ts
@@ -32,3 +32,37 @@ export const newTaskActionVerify = (action: TaskAction): TaskActionVerify => {
     }
     return act
 }
+const VERIFY_TYPE_REGEX = "com.hagoapp.regex"
+const VERIFY_TYPE_PYTHON_SCRIPT = "com.hagoapp.embed.jython"
+const VERIFY_TYPE_NUMBER_RANGE = "com.hagoapp.number.range"
+const VERIFY_TYPE_OPTIONS = "com.hagoapp.options"
+const VERIFY_TYPE_TIME_RANGE = "com.hagoapp.time.range"
+const VERIFY_TYPE_RELATIVE_TIME_RANGE = "com.hagoapp.relative.time.range"
+
+export const VerificationTypes = [
+    { label: "Python Script", value: VERIFY_TYPE_PYTHON_SCRIPT },
+    { label: "Number Range", value: VERIFY_TYPE_NUMBER_RANGE },
+    { label: "Options", value: VERIFY_TYPE_OPTIONS },
+    { label: "Regular Expression Matching", value: VERIFY_TYPE_REGEX },
+    { label: "Time Range", value: VERIFY_TYPE_TIME_RANGE },
+    { label: "Relative Time Range", value: VERIFY_TYPE_RELATIVE_TIME_RANGE },
+]
+
+export const isRegexRule = (ruleConfig: RuleConfig): boolean => {
+    return ruleConfig.configType === VERIFY_TYPE_REGEX
+}
+export const isPythonRule = (ruleConfig: RuleConfig): boolean => {
+    return ruleConfig.configType === VERIFY_TYPE_PYTHON_SCRIPT
+}
+export const isOptionsRule = (ruleConfig: RuleConfig): boolean => {
+    return ruleConfig.configType === VERIFY_TYPE_OPTIONS
+}
+export const isNumberRangeRule = (ruleConfig: RuleConfig): boolean => {
+    return ruleConfig.configType === VERIFY_TYPE_NUMBER_RANGE
+}
+export const isTimeRangeRule = (ruleConfig: RuleConfig): boolean => {
+    return ruleConfig.configType === VERIFY_TYPE_TIME_RANGE
+}
+export const isRelativeTimeRangeRule = (ruleConfig: RuleConfig): boolean => {
+    return ruleConfig.configType === VERIFY_TYPE_RELATIVE_TIME_RANGE
+}
