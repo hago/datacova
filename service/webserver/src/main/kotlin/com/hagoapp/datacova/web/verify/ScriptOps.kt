5e687ad24fcd2df78b9614d6eedd13434337b788
Chaojun Sun <suncjs@hagoapp.com>
2023-05-04 11:45:07 +0800

    remove lua related codes

--- service/webserver/src/main/kotlin/com/hagoapp/datacova/web/verify/ScriptOps.kt
+++ service/webserver/src/main/kotlin/com/hagoapp/datacova/web/verify/ScriptOps.kt
@@ -9,7 +9,6 @@ package com.hagoapp.datacova.web.verify
 
 import com.google.gson.Gson
 import com.hagoapp.datacova.CoVaException
-import com.hagoapp.datacova.util.LuaHelper
 import com.hagoapp.datacova.util.StackTraceWriter
 import com.hagoapp.datacova.util.http.ResponseHelper
 import com.hagoapp.datacova.web.MethodName
@@ -21,55 +20,11 @@ import com.hagoapp.surveyor.utils.EmbedJsFunctionHelper
 import com.hagoapp.surveyor.utils.EmbedPythonHelper
 import io.netty.handler.codec.http.HttpResponseStatus.*
 import io.vertx.ext.web.RoutingContext
-import org.luaj.vm2.LuaError
-import org.luaj.vm2.lib.jse.JsePlatform
 import org.slf4j.LoggerFactory
 
 class ScriptOps {
 
     private val logger = LoggerFactory.getLogger(ScriptOps::class.java)
-
-    @WebEndPoint(
-        methods = [MethodName.POST],
-        path = "/api/lua/evaluate",
-        authTypes = [AuthType.UserToken]
-    )
-    fun evaluateLua(context: RoutingContext) {
-        val data = Gson().fromJson(context.body().asString(), EvaluateData::class.java)
-        if (data.code.isBlank()) {
-            ResponseHelper.sendResponse(
-                context, BAD_REQUEST, mapOf(
-                    "code" to BAD_REQUEST.code(),
-                    "error" to mapOf(
-                        "message" to "Empty code snippet"
-                    )
-                )
-            )
-            return
-        }
-        val code = data.code.replace("\\r", "\r").replace("\\n", "\n")
-        try {
-            val chunk = JsePlatform.debugGlobals().load(code)
-            val arg = LuaHelper.mapToLuaTable(data.fieldValues)
-            val ret = chunk.call(arg)
-            ResponseHelper.sendResponse(
-                context, OK, mapOf(
-                    "code" to 0,
-                    "data" to ret.toString()
-                )
-            )
-        } catch (ex: LuaError) {
-            StackTraceWriter.write(ex, logger)
-            ResponseHelper.respondError(context, BAD_REQUEST, ex.message)
-        } catch (ex: Exception) {
-            ResponseHelper.respondError(context, INTERNAL_SERVER_ERROR, ex.message)
-        }
-    }
-
-    data class EvaluateData(
-        val code: String,
-        val fieldValues: Map<String, String>
-    )
 
     enum class EvalDataType {
         Number,
