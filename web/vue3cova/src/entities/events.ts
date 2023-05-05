fd7e592251c35c495d1ed03f74574f244742da1d
Chaojun Sun <suncjs@hagoapp.com>
2023-05-05 14:01:52 +0800

    define new workspace dialog close event

--- web/vue3cova/src/entities/events.ts
+++ web/vue3cova/src/entities/events.ts
@@ -12,3 +12,5 @@ export const EVENT_CONNECTION_DELETED = "onConnectionDelected"
 export const EVENT_GLOBAL_DRAWER_NOTIFY = "global_drawer_botify"
 
 export const EVENT_TASK_ACTION_DELETE = "task_action_delete"
+
+export const EVENT_NEW_WORKSPACE_DIALOG_CLOSED = "new_workspace_dialog_closed"
