53a1c2efffff6fee9f827611a40216276474f6e2
Chaojun Sun <suncjs@hagoapp.com>
2023-05-25 14:29:46 +0800

    display basic info

--- web/vue3cova/src/components/execution/ExecutionDetail.vue
+++ web/vue3cova/src/components/execution/ExecutionDetail.vue
@@ -1,5 +1,11 @@
 <script lang="ts">
+import executionApiHelper from '@/api/executionapi';
+import { EVENT_REMOTE_API_ERROR } from '@/entities/events';
+import { formatStatus, type ExecutionStatus, type TaskExecution } from '@/entities/execution/taskexecution';
+import { identityStore } from '@/stores/identitystore';
+import { eventBus } from '@/util/eventbus';
 import { defineComponent, reactive } from 'vue';
+import dayjs from 'dayjs'
 
 export default defineComponent({
   props: {
@@ -13,16 +19,53 @@ export default defineComponent({
     }
   },
   setup(props) {
-    return reactive({})
+    return reactive({
+      execution: null as TaskExecution | null
+    })
+  },
+  mounted() {
+    this.loadDetail()
+  },
+  methods: {
+    loadDetail() {
+      let user = identityStore().currentIdentity()
+      executionApiHelper.getExecution(user, this.$props.id).then(rsp => {
+        this.execution = rsp.data
+      }).catch(err => {
+        eventBus.send(EVENT_REMOTE_API_ERROR, err)
+      })
+    },
+    formatTime(x: number | undefined | null): string {
+      if ((x !== undefined) && (x !== null)) {
+        return dayjs(x).format()
+      } else {
+        return ''
+      }
+    },
+    showStatus(x: ExecutionStatus | undefined): string {
+      return x === undefined ? "" : formatStatus(x)
+    }
   }
 })
 </script>
 
 <template>
-  <div>
-    Execution Detail
-  </div>
+  <n-grid :cols="4">
+    <n-gi>id</n-gi>
+    <n-gi class="content">{{ execution?.id }}</n-gi>
+    <n-gi>Task Name</n-gi>
+    <n-gi class="content">{{ execution?.task.name }}</n-gi>
+    <n-gi>Status</n-gi>
+    <n-gi class="content">{{ showStatus(execution?.status) }}</n-gi>
+    <n-gi>Started At</n-gi>
+    <n-gi class="content">{{ formatTime(execution?.startTime) }}</n-gi>
+    <n-gi>End At</n-gi>
+    <n-gi class="content">{{ formatTime(execution?.endTime) }}</n-gi>
+  </n-grid>
 </template>
 
 <style scoped>
+.content {
+  color: aliceblue;
+}
 </style>
