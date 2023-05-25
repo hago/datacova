06979d736138274d88525650c04359904a06fe7b
Chaojun Sun <suncjs@hagoapp.com>
2023-05-25 14:29:29 +0800

    add some util functions

--- web/vue3cova/src/entities/execution/taskexecution.ts
+++ web/vue3cova/src/entities/execution/taskexecution.ts
@@ -6,6 +6,41 @@ import type { Task } from "../task/task"
 
 export type ExecutionStatus = 0 | "0" | 1 | "1" | 2 | "2" | -1 | "-1"
 
+export const isQueueing = (status: ExecutionStatus): boolean => {
+    return status === 0 || status === '0'
+}
+
+export const isInProcess = (status: ExecutionStatus): boolean => {
+    return status === 1 || status === '1'
+}
+
+export const isSucceeded = (status: ExecutionStatus): boolean => {
+    return status === 2 || status === '2'
+}
+
+export const isFailed = (status: ExecutionStatus): boolean => {
+    return status === -1 || status === '-1'
+}
+
+export const formatStatus = (x: ExecutionStatus): string => {
+    switch (x) {
+        case 0:
+        case "0":
+            return "Queueing"
+        case 1:
+        case "1":
+            return "In process"
+        case 2:
+        case "2":
+            return "Succeeded"
+        case -1:
+        case "-1":
+            return "Failed"
+        default:
+            return `Unknown ${x}`
+    }
+}
+
 export interface TaskExecution {
     id: number
     taskId: number
