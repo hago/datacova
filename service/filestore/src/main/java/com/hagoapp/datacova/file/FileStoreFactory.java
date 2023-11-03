45f6b0a8c69a15d9d5a8937a6c1226701127bc26
Chaojun Sun <suncjs@hagoapp.com>
2023-11-03 14:38:22 +0800

    impl factory method to create file store from connection string

--- service/filestore/src/main/java/com/hagoapp/datacova/file/FileStoreFactory.java
+++ service/filestore/src/main/java/com/hagoapp/datacova/file/FileStoreFactory.java
@@ -8,6 +8,7 @@
 package com.hagoapp.datacova.file;
 
 import lombok.extern.slf4j.Slf4j;
+import org.jetbrains.annotations.NotNull;
 import org.reflections.Reflections;
 import org.reflections.scanners.Scanners;
 
@@ -29,8 +30,14 @@ public class FileStoreFactory {
     }
 
     private static final Map<String, Constructor<? extends FileStore>> fileStoreMaps = new HashMap<>(4);
+    private static final Map<String, Constructor<? extends FsConfig>> fsConfigMaps = new HashMap<>(4);
 
     public static void register(String... namespaces) {
+        discoverFileStores(namespaces);
+        discoverFsConfigs(namespaces);
+    }
+
+    private static void discoverFileStores(String... namespaces) {
         for (var namespace : namespaces) {
             var r = new Reflections(namespace, Scanners.SubTypes);
             var configs = r.getSubTypesOf(FileStore.class);
@@ -45,6 +52,27 @@ public class FileStoreFactory {
                         log.info("Sub FileStore: {} is registered with {}", config.getCanonicalName(), a.name());
                     } catch (NoSuchMethodException e) {
                         log.error("Sub FileStore: {} has no constructor with FsConfig", config.getCanonicalName());
+                    }
+                }
+            }
+        }
+    }
+
+    private static void discoverFsConfigs(String... namespaces) {
+        for (var namespace : namespaces) {
+            var r = new Reflections(namespace, Scanners.SubTypes);
+            var configs = r.getSubTypesOf(FsConfig.class);
+            for (var config : configs) {
+                var a = config.getAnnotation(FsScheme.class);
+                if (a == null) {
+                    log.error("Sub FsConfig: {} is not annotated with FsScheme correctly", config.getCanonicalName());
+                } else {
+                    try {
+                        var constructor = config.getConstructor();
+                        fsConfigMaps.put(a.name(), constructor);
+                        log.info("Sub FsConfig: {} is registered with {}", config.getCanonicalName(), a.name());
+                    } catch (NoSuchMethodException e) {
+                        log.error("Sub FsConfig: {} has no constructor with FsConfig", config.getCanonicalName());
                     }
                 }
             }
@@ -75,4 +103,25 @@ public class FileStoreFactory {
         }
     }
 
+    public static FileStore createFileStore(@NotNull String connectionString) {
+        var schemeLen = connectionString.indexOf(':');
+        if (schemeLen <= 0) {
+            log.error("{} is not valid file store string", connectionString);
+            return null;
+        }
+        var scheme = connectionString.substring(0, schemeLen);
+        var constructor = fsConfigMaps.get(scheme);
+        if (constructor == null) {
+            log.error("{} has no registered FileStore", connectionString);
+            return null;
+        }
+        try {
+            var cfg = constructor.newInstance();
+            cfg.loadConnectionString(connectionString);
+            return createFileStore(cfg);
+        } catch (InstantiationException | RuntimeException | IllegalAccessException | InvocationTargetException e) {
+            log.error("Create FileStore for {} failed: {}", scheme, e.getMessage());
+            return null;
+        }
+    }
 }
