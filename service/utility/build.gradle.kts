5908819ee86dde42261bb842a7a4bf41b2d8f291
Chaojun Sun <suncjs@hagoapp.com>
2023-11-10 17:06:58 +0800

    upgrade jedis to 5.0.2

--- service/utility/build.gradle.kts
+++ service/utility/build.gradle.kts
@@ -10,14 +10,15 @@ repositories {
 }
 
 dependencies {
+    implementation(project(":filestore"))
     implementation("com.fasterxml.jackson.core:jackson-databind:2.15.3")
     implementation("com.fasterxml.jackson.jr:jackson-jr-objects:2.15.3")
     implementation("com.google.code.gson:gson:2.10.1")
     implementation("org.slf4j", "slf4j-api", "2.0.9")
     implementation("org.apache.directory.api:api-all:2.1.5")
     api("org.apache.directory.api:api-all:2.1.5")
-    implementation("redis.clients:jedis:3.10.0")
-    api("redis.clients:jedis:3.10.0")
+    implementation("redis.clients:jedis:5.0.2")
+    api("redis.clients:jedis:5.0.2")
     implementation("com.github.albfernandez:juniversalchardet:2.4.0")
     implementation("com.sun.mail", "javax.mail", "1.6.2")
     compileOnly("org.projectlombok:lombok:1.18.30")
