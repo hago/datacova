8b8b75f3cd98ed84c9fde2016ac782564e677b21
Chaojun Sun <suncjs@hagoapp.com>
2023-11-14 13:40:50 +0800

    ...

--- service/build.gradle.kts
+++ service/build.gradle.kts
@@ -2,8 +2,15 @@ plugins {
     java
     kotlin("jvm") version "1.8.22"
     id("org.jetbrains.dokka") version ("1.8.20")
+    id("jacoco")
+    id("org.sonarqube") version "4.3.1.3277"
 }
 
+tasks.jacocoTestReport {
+    reports {
+        xml.required.set(true)
+    }
+}
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib:1.8.22")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.8.22")
@@ -16,7 +23,6 @@ subprojects {
     apply(plugin = "kotlin").apply { version = "1.8.22" }
     dependencies {
         implementation("com.google.code.gson:gson:2.10.1")
-        testImplementation(kotlin("test", "1.8.22"))
     }
 }
 
