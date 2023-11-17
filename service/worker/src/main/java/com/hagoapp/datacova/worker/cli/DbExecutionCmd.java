f9b17257263803c0daaabea595169a153bc3055e
Chaojun Sun <suncjs@hagoapp.com>
2023-11-17 23:48:43 +0800

    impl DbExec

--- service/worker/src/main/java/com/hagoapp/datacova/worker/cli/DbExecutionCmd.java
+++ service/worker/src/main/java/com/hagoapp/datacova/worker/cli/DbExecutionCmd.java
@@ -8,31 +8,32 @@
 package com.hagoapp.datacova.worker.cli;
 
 import com.hagoapp.datacova.lib.data.TaskExecutionData;
+import com.hagoapp.datacova.lib.execution.ExecutionActionDetail;
+import com.hagoapp.datacova.lib.execution.ExecutionDetail;
 import com.hagoapp.datacova.lib.execution.TaskExecution;
-import com.hagoapp.datacova.lib.ingest.TaskActionIngest;
+import com.hagoapp.datacova.utility.StackTraceWriter;
 import com.hagoapp.datacova.worker.Application;
 import com.hagoapp.datacova.worker.Config;
+import com.hagoapp.datacova.worker.Worker;
+import com.hagoapp.datacova.worker.execution.TaskExecutionWatcher;
+import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import picocli.CommandLine;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.nio.charset.StandardCharsets;
 import java.util.concurrent.Callable;
 
 @CommandLine.Command(name = "execdb", description = {"run a task execution loaded from database"})
-public class DbExecutionCmd implements Callable<Integer> {
+public class DbExecutionCmd implements Callable<Integer>, TaskExecutionWatcher {
     @CommandLine.Option(
             names = {"-id", "--id"}, description = {"task execution id"}, required = true
     )
     private int id;
     private final Logger logger = LoggerFactory.getLogger(DbExecutionCmd.class);
     private final Config config = Application.oneApp().getConfig();
-    private TaskExecution taskExecution;
 
     @Override
-    public Integer call() throws Exception {
+    public Integer call() {
         if (config == null) {
             logger.error("No config found, exit");
             return -1;
@@ -41,12 +42,66 @@ public class DbExecutionCmd implements Callable<Integer> {
             logger.error("No database config found, exit");
             return -2;
         }
+        TaskExecution taskExecution;
         try (var db = new TaskExecutionData(config.getDb())) {
             taskExecution = db.getTaskExecution(id);
             if (taskExecution == null) {
                 logger.error("task execution with id {} not found, exit", id);
+                return -3;
             }
         }
+        var worker = new Worker(taskExecution);
+        worker.addWatcher(this);
+        worker.execute();
         return 0;
     }
+
+    /* Watcher for execution */
+    @Override
+    public void onStart(@NotNull TaskExecution te) {
+        //
+    }
+
+    @Override
+    public void onComplete(@NotNull TaskExecution te, @NotNull ExecutionDetail result) {
+        if (result.isSucceeded()) {
+            logger.info("SUCCESS");
+        } else {
+            logger.error("FAILED");
+        }
+        logger.info("detail: {}", result);
+    }
+
+    @Override
+    public void onError(@NotNull TaskExecution te, @NotNull Exception error) {
+        logger.error("execution failed");
+        StackTraceWriter.write(error, logger);
+    }
+
+    @Override
+    public void onActionStart(@NotNull TaskExecution te, int actionIndex) {
+        //
+    }
+
+    @Override
+    public void onActionComplete(@NotNull TaskExecution te, int actionIndex, @NotNull ExecutionActionDetail result) {
+        //
+    }
+
+    @Override
+    public void onActionError(@NotNull TaskExecution te, int actionIndex, @NotNull Exception error) {
+        //
+    }
+
+    @Override
+    public void onDataLoadStart(@NotNull TaskExecution te) {
+        //
+    }
+
+    @Override
+    public void onDataLoadComplete(@NotNull TaskExecution te, boolean isLoadingSuccessful) {
+        //
+    }
+    /* End of watcher for execution */
+
 }
