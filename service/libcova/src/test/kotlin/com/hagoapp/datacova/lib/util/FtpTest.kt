2d170b221ee4065f26e3c82a3a4bf19288c72140
Chaojun Sun <suncjs@hagoapp.com>
2023-12-11 17:30:38 +0800

    add ftp test with stream

--- service/libcova/src/test/kotlin/com/hagoapp/datacova/lib/util/FtpTest.kt
+++ service/libcova/src/test/kotlin/com/hagoapp/datacova/lib/util/FtpTest.kt
@@ -10,17 +10,24 @@ package com.hagoapp.datacova.lib.util
 import com.hagoapp.datacova.lib.distribute.conf.FtpConfig
 import com.hagoapp.datacova.lib.util.FtpClient
 import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
+import java.io.ByteArrayInputStream
 import java.util.UUID
 
 class FtpTest {
-    @Test
-    fun testFtp() {
-        val config = FtpConfig()
+
+    private val config: FtpConfig = FtpConfig()
+
+    init {
         config.host = "ftp.dlptest.com"
         config.port = 21
         config.login = "dlpuser"
         config.password = "rNrKYTX9g7z3RgJRmxWuGHbeu"
+    }
+
+    @Test
+    fun testFtp() {
         FtpClient(config).use { it ->
             val entries = it.list()
             Assertions.assertNotNull(entries)
@@ -34,6 +41,29 @@ class FtpTest {
             }
             val fn = UUID.randomUUID().toString()
             it.put(fn, fn.toByteArray())
+            val content = String(it.get(fn))
+            Assertions.assertEquals(fn, content)
+            it.delete(fn)
+        }
+    }
+
+    @Test
+    fun testFtpStreamSource() {
+        FtpClient(config).use { it ->
+            val entries = it.list()
+            Assertions.assertNotNull(entries)
+            val dir = entries.firstOrNull() { entry -> entry.isDirectory }
+            if (dir != null) {
+                it.cd(dir.name)
+                val pwd = it.pwd()
+                Assertions.assertEquals("/" + dir.name, pwd)
+                it.cd("../")
+                Assertions.assertEquals("/", it.pwd())
+            }
+            val fn = UUID.randomUUID().toString()
+            ByteArrayInputStream(fn.toByteArray()).use { stream ->
+                it.put(fn, stream)
+            }
             val content = String(it.get(fn))
             Assertions.assertEquals(fn, content)
             it.delete(fn)
